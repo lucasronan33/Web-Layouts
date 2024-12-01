932f32661a074860f2f4b5bcb4d732b2009953f1
lucasronan33 <lucasronan33@gmail.com>
2024-12-01 16:39:04 -0300

    backgroud alterado dinamicamente com JS

--- src/script.js
+++ src/script.js
@@ -1,15 +1,49 @@
 const label_tipoImpressao = document.getElementById('label_slct_tipo_impressao')
 const slct_tipo_impressao = document.getElementById('slct_tipo_impressao')
-
 // onchange=(event)=>{}
 
 slct_tipo_impressao.addEventListener('change', (event) => {
+    const tipoSelecionado = slct_tipo_impressao.value
+
     console.log(slct_tipo_impressao.value)
     if (slct_tipo_impressao.value !== 'recorte') {
         label_tipoImpressao.style.display = 'block'
     } else {
         label_tipoImpressao.style.display = 'none'
     }
+
+    let formCorpo = document.querySelector('.corpo')
+
+    switch (tipoSelecionado) {
+        case 'uv':
+            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_uv.png)'
+            console.log('Background alterado')
+            break;
+
+        case 'adesivo':
+            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_adesivo.png)'
+            console.log('Background alterado')
+            break;
+
+        case 'recorte':
+            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_recorte.png)'
+            console.log('Background alterado')
+            break;
+
+        case 'tecido':
+            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_tecido.png)'
+            console.log('Background alterado')
+            break;
+
+        case 'lona':
+            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_lona.png)'
+            console.log('Background alterado')
+            break;
+
+        default:
+            break;
+    }
+
 })
 
 
