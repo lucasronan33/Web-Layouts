b608e0667ddc533ddc59a877b1dc0ad697dc821a
lucasronan33 <lucasronan33@gmail.com>
2024-12-03 23:52:25 -0300

    criando grid .informacoes_layout

--- src/script.js
+++ src/script.js
@@ -16,50 +16,79 @@ slct_tipo_impressao.addEventListener('change', (event) => {
     // alteração de gabarito de background
     let formCorpo = document.querySelector('.corpo')
     info_adicionalStyle = document.querySelector('.info_adicional').style
+    let classeElemento
 
+    let i = 6
+    function esconderEstiloBotao() {
+        let esconder = formCorpo.getElementsByClassName('estilo_botao')
+        for (i; i < esconder.length; i++) {
+
+            if (i > 8) {
+                break
+            }
+            console.log('for:', esconder.length)
+            esconder[i].style.display = 'none'
+
+        }
+    }
+    function mostrarEstiloBotao() {
+        let esconder = formCorpo.getElementsByClassName('estilo_botao')
+        for (i; i < esconder.length; i++) {
+
+            if (i > 8) {
+                break
+            }
+            console.log('for:', esconder.length)
+            esconder[i].style.display = 'flex'
+
+        }
+    }
     switch (tipoSelecionado) {
         case 'uv':
             formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_uv.png)'
             console.log('Background alterado')
+            i = 6
+            console.log('i: ' + i)
+
+            mostrarEstiloBotao()
+
             break;
 
         case 'adesivo':
             formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_adesivo.png)'
             console.log('Background alterado')
+            mostrarEstiloBotao()
             break;
 
         case 'recorte':
             formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_recorte.png)'
             console.log('Background alterado')
+
+            console.log('i: ' + i)
+            mostrarEstiloBotao()
+            i = 7
+            esconderEstiloBotao()
+
             break;
 
         case 'tecido':
-            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_tecido.png)'
+            formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_lona.png)'
             console.log('Background alterado')
+
+            mostrarEstiloBotao()
+            i = 6
+            esconderEstiloBotao()
+
             break;
 
         case 'lona':
             formCorpo.style.backgroundImage = 'url(../img/gabarito_layout_lona.png)'
             console.log('Background alterado')
 
-            let esconder = formCorpo.getElementsByClassName('estilo_botao')
+            mostrarEstiloBotao()
+            i = 6
+            esconderEstiloBotao()
 
-            let i
-            for (i = 6; i < esconder.length; i++) {
-
-                if (i > 8) {
-                    break
-                }
-                console.log('for:', esconder.length)
-                esconder[i].style.display = 'none'
-
-            }
-
-            // info_adicionalStyle.marginBottom =
-
-            break;
-
-        default:
             break;
     }
 
