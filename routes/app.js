d5210f441ef6f92087ee8ef32ea2214b1093e35f
Lucas Torres <lucasronan33@gmail.com>
2025-04-28 13:14:44 -0300

    ajustando fontes Quantidade

--- routes/app.js
+++ routes/app.js
@@ -3,7 +3,7 @@ const fs = require("fs"); //biblioteca nativa Node
 const path = require("path"); //biblioteca nativa Node
 
 const app = express();
-const port = 3000;
+const port = 4000;
 
 // Configurando engine EJS
 app.set("view engine", "ejs");
